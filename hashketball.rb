50b8981221cf8f3d78f14603a3ac113bb371d647
Peter Werner <pcw1995@yahoo.com>
2019-09-17 16:45:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,6 +17,9 @@ def game_hash
 end 
 
 def num_points_scored(name)
+  game_hash.each_key do |key1|
+    game_hash[key1].each_key do |key2
+  end 
   p game_hash[:away][:players][0][name][:points]
 end 
 
