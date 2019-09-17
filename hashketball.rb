320676a2cba5fec4a95dac4c93da7c17f0119491
Peter Werner <pcw1995@yahoo.com>
2019-09-17 17:04:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -22,7 +22,7 @@ def num_points_scored(name)
     while game_hash[key1][:players].length > i do  
       game_hash[key1][:players][i].each_key do |key2|
         if key2 == name 
-          p game_hash[:away][:players][i][name][:points] 
+          game_hash[:away][:players][i][name][:points] 
         end 
       end 
       i +=1 
